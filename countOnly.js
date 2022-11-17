21ae4936aecfaa731303904ba37b07c40ea5e060
stephsteph123 <stephaniepotterrowe@gmail.com>
2022-11-17 18:36:55 -0500

    countOnly.js add

---
+++ countOnly.js
@@ -0,0 +1,32 @@
+
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {}
+
+  for (const item of allItems) {
+    console.log(item);
+  }
+  return results
+}
+
+//report of all strings found in the input array and count for each.
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
