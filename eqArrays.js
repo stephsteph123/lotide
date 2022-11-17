bbd13f269ad8ee10c64ceab928087029f9e5f01d
stephsteph123 <stephaniepotterrowe@gmail.com>
2022-11-17 14:44:43 -0500

    update to eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,20 @@
+
+function eqArrays(first,second) {
+  if (first.length !== second.length) {
+    console.log(false)
+    return;
+  }
+  for (let i = 0;i < first.length; i++) {
+    if (first[i] !== second[i]) {
+      console.log(false)
+      return;
+    }
+  }
+  console.log(true)
+}
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
