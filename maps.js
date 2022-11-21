79d77ba48904aa811278f395988fae207e81409c
stephsteph123 <stephaniepotterrowe@gmail.com>
2022-11-20 20:16:45 -0500

    adding maps.js

---
+++ maps.js
@@ -0,0 +1,9 @@
+const map = function() {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
