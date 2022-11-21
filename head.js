81ece4f2b3a67836d460264108265bf5c708c1b3
stephsteph123 <stephaniepotterrowe@gmail.com>
2022-11-21 17:33:03 -0500

    head update

--- head.js
+++ head.js
@@ -1,14 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed:  ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 function head(start) {
-  return(start[0])
-}
+  return start[0];
+  };
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
