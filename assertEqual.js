ea28a168b03ae11a62c24fe173265719605a73aa
stephsteph123 <stephaniepotterrowe@gmail.com>
2022-11-21 17:32:29 -0500

    update to under .module

--- assertEqual.js
+++ assertEqual.js
@@ -7,7 +7,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2);
-assertEqual("Bootcamp", "Bootcamp");
+module.exports = assertEqual;
